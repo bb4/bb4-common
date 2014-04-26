b750d96634768f4dd17e1bc120c248d340d72d04
Barry G. Becker <barrybecker4@yahoo.com>
2014-04-26 05:24:58 -0700

    Add method for reading text files.

--- source/com/barrybecker4/common/util/FileUtil.java
+++ source/com/barrybecker4/common/util/FileUtil.java
@@ -112,4 +112,36 @@ public final class FileUtil {
         }
         return stream;
     }
+
+
+    /**
+     * @param filename name of file to read from
+     * @return text within the file
+     * @throws IllegalStateException if could not read the file
+     */
+    public static String readTextFile(String filename) {
+        BufferedReader br = null;
+        StringBuilder bldr = new StringBuilder(1000);
+
+        try {
+            br = new BufferedReader(new FileReader(filename));
+
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                bldr.append(sCurrentLine).append('\n');
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not read " + filename, e);
+        }
+        finally {
+            try {
+                if (br != null) br.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return bldr.toString();
+    }
 }
