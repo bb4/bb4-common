ac9e2bb3cbfe870bee3203379b695f1f6ca7b6c5
barrybecker4 <barrybecker4@yahoo.com>
2017-11-21 10:04:18 -0800

    Refactor our Jenkinsfile stages to common pipeline library so that all projects can reuse it.

---
+++ vars/commonJenkinsPipeline.groovy
@@ -0,0 +1,64 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('Checkout source') {
+                steps {
+                    git url: pipelineParams.gitUrl, branch: 'master'
+                }
+            }
+
+            stage('build') {
+                steps {
+                    gradleCmd("clean build -x test --refresh-dependencies")
+                }
+            }
+
+            stage('test') {
+                steps {
+                    gradleCmd("test")
+                }
+            }
+
+            stage('documentation') {
+                steps {
+                    gradleCmd("javadoc")
+                }
+            }
+
+            stage('publish') {
+                steps {
+                    gradleCmd("publishArtifacts --info --refresh-dependencies")
+                }
+            }
+        }
+        post {
+            always {
+                junit 'build/test-results/test/*.xml'
+                step([$class: 'JavadocArchiver', javadocDir: 'build/docs/javadoc', keepAll: true])
+            }
+            success {
+                mail to: 'barrybecker4@gmail.com',
+                        subject: "Successful Pipeline: ${currentBuild.fullDisplayName}",
+                        body: "This build succeeded: ${env.BUILD_URL}"
+            }
+            failure {
+                mail to: 'barrybecker4@gmail.com',
+                        subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+                        body: "Something is wrong with ${env.BUILD_URL}"
+            }
+            unstable {
+                echo 'This build is unstable.'
+            }
+        }
+    }
+}
+
+def gradleCmd(cmd) {
+    if (isUnix()) {
+        sh "./gradlew ${cmd}"
+    } else {
+        bat "./gradlew.bat ${cmd}"
+    }
+}
